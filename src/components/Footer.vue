ca9852154faf0f0bc08f65568df1d4c756e1c876
Sandra Rampal <sandrarampal@macbookpro.home>
2025-09-12 13:37:34 +0200

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<script></script>
+
+<template>
+  <footer>
+    <p>
+      Made with <a href="https://vuejs.org/">Vue</a> by
+      <a href="https://github.com/sandrarampal" target="_blank">Sandra Rampal</a>
+    </p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  text-align: center;
+  border-top: 1px solid #ccc;
+}
+
+footer a {
+  color: var(--main-yellow);
+  text-decoration: none;
+}
+</style>
